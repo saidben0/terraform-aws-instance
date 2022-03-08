48160151c965b98295ab84b555e8694d181c9b2b
sben <saidben@amazon.com>
2022-03-08 12:56:43 -0600

    first commit

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "allow_ssh" {
   description = "Allow ssh inbound traffic"
   #vpc_id      = aws_vpc.main.id
   ingress {
-    description      = "SSH from VPC"
+    description      = "SSH from Local"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
