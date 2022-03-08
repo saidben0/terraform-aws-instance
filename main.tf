006a8a2076c389f9dce8d52a65ed5fd670e233a4
sben <saidben@amazon.com>
2022-03-08 14:54:45 -0600

    update the ami to use red hat

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ resource "aws_key_pair" "ssh_key" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.redhat.id
   instance_type = var.instance_type
+  availability_zone = var.availability_zone
   key_name = var.key_name
   security_groups = [ aws_security_group.allow_ssh.name ]
   tags = {
